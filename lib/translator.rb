a800d64b7fcbeb51ee78569d5729473d6bd2a207
agile-prototype-8421 <tylergreason@gmail.com>
2019-12-11 16:21:39 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,6 +5,20 @@ def load_library(path)
   emoticons = YAML.load_file(path)
   
   
+emoticons[:get_meaning] = {}
+emoticons[:get_emoticon] = {}
+emoticons.each do |key,val|
+    english_key = val[0]
+    japanese_key = val[1] 
+
+    if japanese_key != nil 
+        emoticons[:get_meaning][japanese_key] = key
+        emoticons[:get_emoticon][english_key] = key
+    end
+
+    # emoticons[:get_value] = val 
+    
+end
   
   return emoticons 
 end
